42590284d69dedf483cf68ee44aa16b735b07eb9
bernous <bernous@gmail.com>
2024-09-16 18:09:38 +0100

    feat: resize big images " " git commit -m "feat: resize big images"

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,22 +1,83 @@
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaTiktok,
+  FaMapMarkerAlt,
+} from "react-icons/fa";
+
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 py-8 text-white">
-      <div className="container mx-auto px-6 text-center">
-        <p className="text-sm">
-          &copy; {new Date().getFullYear()} Joker Graphics. All rights reserved.
-        </p>
-        <div className="mt-4">
-          <a href="#about" className="mx-2 text-blue-400 hover:text-blue-600">
-            About
-          </a>
-          <a
-            href="#services"
-            className="mx-2 text-blue-400 hover:text-blue-600"
-          >
-            Services
-          </a>
-          <a href="#contact" className="mx-2 text-blue-400 hover:text-blue-600">
-            Contact
+    <footer className="bg-gray-900 py-10 text-white">
+      <div className="container mx-auto grid grid-cols-1 gap-8 px-6 text-center md:grid-cols-3 md:text-left">
+        {/* Company Info */}
+        <div>
+          <h3 className="text-lg font-bold">Joker Graphics</h3>
+          <p className="mt-2 text-sm">
+            &copy; {new Date().getFullYear()} Joker Graphics. All rights
+            reserved.
+          </p>
+          <div className="mt-2 flex items-center justify-center md:justify-start">
+            <FaMapMarkerAlt className="mr-2" />
+            <p className="text-sm">lasas Azzaba, Skikda</p>
+          </div>
+          <p className="mt-2 text-sm">Phone: +213 555 132 188</p>
+        </div>
+
+        {/* Navigation Links */}
+        <div>
+          <h3 className="text-lg font-bold">Quick Links</h3>
+          <div className="mt-4">
+            <a
+              href="#about"
+              className="mx-2 block text-blue-400 hover:text-blue-600"
+            >
+              About
+            </a>
+            <a
+              href="#services"
+              className="mx-2 block text-blue-400 hover:text-blue-600"
+            >
+              Services
+            </a>
+            <a
+              href="#contact"
+              className="mx-2 block text-blue-400 hover:text-blue-600"
+            >
+              Contact
+            </a>
+          </div>
+        </div>
+
+        {/* App Availability & Social Media */}
+        <div>
+          <h3 className="text-lg font-bold">Stay Connected</h3>
+          <div className="mt-4 flex justify-center md:justify-start">
+            <a
+              href="https://facebook.com"
+              className="mx-2 text-blue-600 hover:text-blue-800"
+            >
+              <FaFacebookF className="text-2xl" />
+            </a>
+            <a
+              href="https://instagram.com"
+              className="mx-2 text-pink-600 hover:text-pink-800"
+            >
+              <FaInstagram className="text-2xl" />
+            </a>
+            <a
+              href="https://tiktok.com"
+              className="mx-2 text-black hover:text-gray-800"
+            >
+              <FaTiktok className="text-2xl" />
+            </a>
+          </div>
+          <p className="mt-4 text-sm">Available on Google Play:</p>
+          <a href="#" className="mt-4 inline-block">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/7/78/Google_Play_Store_badge_EN.svg"
+              alt="Google Play Store"
+              className="mx-auto h-12 md:mx-0"
+            />
           </a>
         </div>
       </div>
--- src/components/textFontFamily/TextFontFamily.tsx
+++ src/components/textFontFamily/TextFontFamily.tsx
@@ -1,34 +1,47 @@
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { canvasActions } from "../../store/slices/canvasSlice";
 import { IRootState } from "../../store/store";
-import {
-  getCurrentSelectedText,
-} from "../../store/selectors/canvasSelectors";
+import { getCurrentSelectedText } from "../../store/selectors/canvasSelectors";
 
 const TextFontFamily = () => {
   const dispatch = useDispatch();
   const { selectedLayer } = useSelector((state: IRootState) => state.canvas);
-
   const text = useSelector((state) => getCurrentSelectedText(state));
+
+  const [fontFamily, setFontFamily] = useState(text?.fontFamily || "");
+
   const fonts = [
-    { name: "Grey Qo", value: '"Grey Qo", cursive' },
-    { name: "Inter", value: '"Inter", sans-serif' },
-    { name: "Lora", value: '"Lora", serif' },
-    { name: "Moderustic", value: '"Moderustic", sans-serif' },
-    { name: "New Amsterdam", value: '"New Amsterdam", sans-serif' },
+    { name: "Bebas Neue", value: '"Bebas Neue", sans-serif' },
+    { name: "Anton", value: '"Anton", sans-serif' },
+    { name: "Impact", value: '"Impact", sans-serif' },
+    { name: "Lobster", value: '"Lobster", cursive' },
+    { name: "Pacifico", value: '"Pacifico", cursive' },
+    { name: "Montserrat", value: '"Montserrat", sans-serif' },
+    { name: "Oswald", value: '"Oswald", sans-serif' },
+    { name: "Playfair Display", value: '"Playfair Display", serif' },
+    { name: "Raleway", value: '"Raleway", sans-serif' },
+    { name: "Fira Sans", value: '"Fira Sans", sans-serif' },
   ];
 
+  const handleFontChange = (e) => {
+    const newFont = e.target.value;
+    setFontFamily(newFont);
+
+    setTimeout(() => {
+      dispatch(
+        canvasActions.editText({
+          id: selectedLayer?.id,
+          fontFamily: newFont,
+        }),
+      );
+    }, 1000);
+    console.log(newFont);
+  };
   return (
     <select
-      value={text?.fontFamily}
-      onChange={(e) =>
-        dispatch(
-          canvasActions.editText({
-            id: selectedLayer?.id,
-            fontFamily: e.target.value,
-          }),
-        )
-      }
+      value={fontFamily}
+      onChange={handleFontChange}
       className="cursor-pointer rounded-xl px-4 py-1"
     >
       {fonts.map((font, index) => (
@@ -39,4 +52,5 @@ const TextFontFamily = () => {
     </select>
   );
 };
+
 export default TextFontFamily;
