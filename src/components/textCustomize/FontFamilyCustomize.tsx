a98da65b62b54564b7ce662db084995ba9505d27
bernous <bernous@gmail.com>
2024-09-19 12:16:41 +0100

    feat:add more fonts

--- src/components/textCustomize/FontFamilyCustomize.tsx
+++ src/components/textCustomize/FontFamilyCustomize.tsx
@@ -13,11 +13,16 @@ const FontFamilyCustomize = ({ canvasText }: FontFamilyCustomizeProps) => {
   const dispatch = useDispatch();
 
   const fonts = [
-    { name: "Grey Qo", value: '"Grey Qo", cursive' },
-    { name: "Inter", value: '"Inter", sans-serif' },
-    { name: "Lora", value: '"Lora", serif' },
-    { name: "Moderustic", value: '"Moderustic", sans-serif' },
-    { name: "New Amsterdam", value: '"New Amsterdam", sans-serif' },
+    { name: "Normal", value: "sans-serif" },
+    { name: "Dancing Script", value: '"Dancing Script", sans-serif' },
+    { name: "Impact", value: '"Impact", sans-serif' },
+    { name: "Lobster", value: '"Lobster", cursive' },
+    { name: "Pacifico", value: '"Pacifico", cursive' },
+    { name: "Montserrat", value: '"Montserrat", sans-serif' },
+    { name: "Oswald", value: '"Oswald", sans-serif' },
+    { name: "Playfair Display", value: '"Playfair Display", serif' },
+    { name: "Raleway", value: '"Raleway", sans-serif' },
+    { name: "Fira Sans", value: '"Fira Sans", sans-serif' },
   ];
 
   const handleFontChange = async (fontFamily: string) => {
